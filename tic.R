259d80fcc3c39010d04391f950c3e603cab0c9ae
amarin <17020181+asiripanich@users.noreply.github.com>
2019-11-28 12:44:33 +1100

    use pkgdown and travis without using the tic package

--- tic.R
+++
@@ -1,7 +0,0 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks()
-
-if (ci_has_env("BUILD_PKGDOWN")) {
-  # creates pkgdown site and pushes to gh-pages branch
-  do_pkgdown(orphan = TRUE)
-}
