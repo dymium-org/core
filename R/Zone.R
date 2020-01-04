38d185f10b2331241640306f13d2c0467abd0b4c
amarin <17020181+asiripanich@users.noreply.github.com>
2020-01-05 02:48:33 +1100

    refactor(Zone): remove default `id_col` from `new()` and data_template

--- R/Zone.R
+++ R/Zone.R
@@ -11,18 +11,9 @@ Zone <- R6::R6Class(
   classname = "Zone",
   inherit = Environment,
   public = list(
-
-    data_template = function() {
-      data.table(
-        zid = integer(),
-        zname = character()
-      )
-    },
-
-    initialise_data = function(data, id_col = "zid") {
+    initialise_data = function(data, id_col) {
       super$initialise_data(data, id_col)
       invisible()
     }
-
   )
 )
