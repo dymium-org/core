fc7cd7f0e7e34a70e46add31e44ff1a47348b4b7
amarin <17020181+asiripanich@users.noreply.github.com>
2020-01-27 20:00:34 +1100

    test: fix Target

--- tests/testthat/test-Target.R
+++ tests/testthat/test-Target.R
@@ -1,7 +1,7 @@
 test_that("Target", {
   Tgt <- Target$new(list(yes = 10))
   expect_equal(Tgt$data, list(yes = 10))
-  expect_equal(Trgt$get(), list(yes = 10))
+  expect_equal(Tgt$get(), list(yes = 10))
   expect_false(Tgt$dynamic)
 
   target_dynamic <- data.table(time = 1:10, yes = 10)
@@ -9,4 +9,5 @@ test_that("Target", {
   expect_equal(TgtDy$get(), list(yes = 10))
   expect_equal(TgtDy$data, target_dynamic)
   expect_true(TgtDy$dynamic)
+  expect_target(TgtDy)
 })
