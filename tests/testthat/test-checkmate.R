977cb03e2d82bf5ae7a46575e36aca11b3e11ff2
amarin <17020181+asiripanich@users.noreply.github.com>
2020-01-27 20:30:35 +1100

    test: fix check_target test

--- tests/testthat/test-checkmate.R
+++ tests/testthat/test-checkmate.R
@@ -17,7 +17,7 @@ test_that("check_target", {
   expect_error(check_target(data.frame(time = 1:10)))
 
   expect_error(check_target(data.frame(time = rep(1, 10))),
-               regexp = "Must have at least 2 cols, but has 1 cols")
+               regexp = "Must have at least 2 cols, but has 1")
 
   expect_error(check_target(data.frame(time = paste(1:10))))
 
