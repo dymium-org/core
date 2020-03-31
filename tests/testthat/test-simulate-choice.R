6f928acae22ad985bd22222b0eb02ee9577b1305
amarin <17020181+asiripanich@users.noreply.github.com>
2020-03-31 17:44:32 +1100

    test: remove mboost

--- tests/testthat/test-simulate-choice.R
+++ tests/testthat/test-simulate-choice.R
@@ -37,7 +37,7 @@ test_that("simulate_choice.data.frame works", {
 test_that("simulate_choice.WrappedModel from mlr works", {
 
   # two classes
-  if (requireNamespace("mlr") & requireNamespace("mboost")) {
+  if (requireNamespace("mlr")) {
     train_mod <- create_mlr_binary_model()
     checkmate::expect_character(simulate_choice(train_mod, newdata = toy_individuals),
                                 pattern = "male|female",
