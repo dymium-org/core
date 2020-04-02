298cfc9510b181d0d1a524186adde9000ee2fe88
amarin <17020181+asiripanich@users.noreply.github.com>
2020-04-02 16:04:06 +1100

    add: mutate_entity accepts a preprocessing function as a filtering strategy.

--- R/mutate-entity.R
+++ R/mutate-entity.R
@@ -6,13 +6,15 @@
 #' inheritances stored in [World] to be mutated.
 #'
 #' It is useful for updating attribute variables, such as age of entities in each
-#' iteration etc.
+#' iteration etc. Note that, only one or none of the filter strategies
+#' {ids, subset, preprocessing_fn} should be used at a time.
 #'
 #' @param world a [World] object.
 #' @param entity a character denoting an entity classname.
 #' @param ... a mutate expression using [data.table] syntax, e.g: age := age +1
 #' @param ids a integer vector containing ids of `entity` to be mutated.
 #' @param subset a subset expression, e.g: age > 30.
+#' @param preprocessing_fn a filter function that accepts a data.table object.
 #'
 #' @return a [World] object.
 #' @export
@@ -32,17 +34,21 @@
 #' for (i in 1:10) {
 #'   world %>% mutate_entity(entity = "Individual", age := age + 1L, subset = sex == "female")
 #' }
-mutate_entity <- function(world, entity, ..., ids = NULL, subset) {
+mutate_entity <- function(world, entity, ..., ids = NULL, subset, preprocessing_fn) {
   checkmate::assert_r6(world, classes = "World")
   if (!checkmate::test_choice(entity, names(world$entities))) {
     stop("'", entity, "' not found in `world`.")
   }
-  if ((!is.null(ids) & !missing(subset))) {
-    stop("Only ids or subset should be given but never both!")
+  if (sum(c(!is.null(ids), !missing(subset), !missing(preprocessing_fn))) > 1) {
+    stop("Only one or none of the filter parameters {ids, subset, and propressing_fn} should be used.")
   }
   e <- world$get(entity)
-  if (is.null(ids) & missing(subset)) {
+  if (is.null(ids) & missing(subset) & missing(preprocessing_fn)) {
     e$get_data(copy = FALSE)[, ...]
+  }
+  if (!missing(preprocessing_fn)) {
+    checkmate::assert_function(preprocessing_fn, nargs = 1, null.ok = FALSE)
+    ids <- preprocessing_fn(e$get_data(copy = TRUE))[[e$get_id_col()]]
   }
   if (!is.null(ids)) {
     assert_entity_ids(e, ids, informative = T)
