cacc16f97fc0c30d4eebd6f1c72facd0ce6939b3
amarin <17020181+asiripanich@users.noreply.github.com>
2020-05-18 21:54:45 +1000

    test: replaced skip_on_travis with skip_on_ci since we now use GH actions

--- tests/testthat/test-module.R
+++ tests/testthat/test-module.R
@@ -1,11 +1,11 @@
 test_that("check_module", {
-  skip_on_travis()
+  skip_on_ci()
   skip_on_not_master()
   expect_true(check_module("demography"))
 })
 
 test_that("get_modules", {
-  skip_on_travis()
+  skip_on_ci()
   skip_on_not_master()
   checkmate::expect_names(get_modules(), must.include = c("demography", "matsim", "test"))
 })
@@ -24,7 +24,7 @@ test_that(".filter_zip_versions", {
 })
 
 test_that("get_module", {
-  skip_on_travis()
+  skip_on_ci()
   skip_on_not_master()
   expect_error(download_module(name = "test", version = "10.0.0", force = T, remove_download = T),
                regexp = "The requested version of test module doesn't exist.")
