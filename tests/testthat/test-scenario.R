17286007fa29eb427ccc715696d19c86d8102e25
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-23 16:54:24 +1100

    removed scenario-related files

--- tests/testthat/test-scenario.R
+++
@@ -1,6 +0,0 @@
-test_that("get_active_scenario", {
-  checkmate::expect_list(get_active_scenario(), types = "character", any.missing = FALSE)
-  checkmate::expect_access(get_active_scenario()[['scenario_dir']])
-  checkmate::expect_access(get_active_scenario()[['output_dir']])
-  checkmate::expect_access(get_active_scenario()[['input_dir']])
-})
